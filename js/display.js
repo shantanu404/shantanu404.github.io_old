301acc0ac4cbda7f85ccd7ecbf9d8ee9f62f0c38
Shantanu Rahman <rahmanshantanu69@gmail.com>
2016-10-09 21:23:25 +0600

    Now we are responsive enough... #LikeABoss B)

--- js/display.js
+++ js/display.js
@@ -5,7 +5,21 @@
         el.addEventListener('click', function (e) {
             document.querySelector('.active').className = 'nav-item';
             this.className = 'nav-item active'
+            try {
+                document.querySelector('.navigation.nav-active').className = 'navigation';
+            } catch (e) {
+                // it's probably a desktop
+            }
         });
     }
+
+    document.getElementById('nav-btn').addEventListener('click', function (e) {
+        var navigation = document.querySelector('.navigation');
+        if (navigation.className.includes('nav-active')) {
+            navigation.className = 'navigation';
+        } else {
+            navigation.className = 'navigation nav-active';
+        }
+    });
 
 })();
